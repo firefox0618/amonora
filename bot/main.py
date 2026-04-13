469b56e9a11a0a68f0026216048eef4d03c7620a
Rudolf <rudolfnaumow@gmail.com>
2026-04-14 02:06:19 +0500

    Исправил регистрацию роутеров

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,14 @@ from aiogram.types import BotCommand, MenuButtonCommands
 
 from backend.core.schema import ensure_schema
 from bot.config import config
+from bot.handlers.cabinet import router as cabinet_router
+from bot.handlers.devices import router as devices_router
+from bot.handlers.info import router as info_router
+from bot.handlers.protocol import router as protocol_router
+from bot.handlers.referrals import router as referrals_router
+from bot.handlers.start import router as start_router
+from bot.handlers.support import router as support_router
+from bot.handlers.tariffs import router as tariffs_router
 from bot.middlewares.activity import UserActivityMiddleware
 from bot.utils.logging_setup import configure_logging
 from test_bot.router import router as v2_router
@@ -12,6 +20,14 @@ from test_bot.router import router as v2_router
 
 dp = Dispatcher()
 dp.include_router(v2_router)
+dp.include_router(start_router)
+dp.include_router(cabinet_router)
+dp.include_router(devices_router)
+dp.include_router(protocol_router)
+dp.include_router(info_router)
+dp.include_router(referrals_router)
+dp.include_router(support_router)
+dp.include_router(tariffs_router)
 
 
 async def main() -> None:
